9c71e3acf3bea517b6f5b89d36cabdd3df25da85
Kelton Hale <77764287+khale913@users.noreply.github.com>
2022-09-26 15:01:37 -0700

    project layout init

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,25 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LandingComponent } from './components/landing/landing.component';
+import { PartnersComponent } from './components/partners/partners.component';
+import { TestimonialsComponent } from './components/testimonials/testimonials.component';
+import { ServicesComponent } from './components/services/services.component';
+import { DemoComponent } from './components/demo/demo.component';
+import { UnknownPageComponent } from './components/unknown-page/unknown-page.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LandingComponent,
+    PartnersComponent,
+    TestimonialsComponent,
+    ServicesComponent,
+    DemoComponent,
+    UnknownPageComponent,
+    NavbarComponent,
+
   ],
   imports: [
     BrowserModule,
