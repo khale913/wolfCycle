b0c818440de4a18d819a62fec65e0f6aeacf6202
Kelton Hale <77764287+khale913@users.noreply.github.com>
2022-09-26 15:40:38 -0700

    select active tab

--- src/app/components/navbar/navbar.component.ts
+++ src/app/components/navbar/navbar.component.ts
@@ -6,14 +6,14 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./navbar.component.scss']
 })
 export class NavbarComponent implements OnInit {
-
+  activeTab: string = 'home';
   constructor() { }
 
   ngOnInit(): void {
   }
 
   navigateSection(section: string) {
-
+    this.activeTab = section;
     document.getElementById(section)?.scrollIntoView({
       behavior: "smooth",
       block: "start",
