06658b87b7a10ee27355969d52e6e03fa7b5342c
Kelton Hale <77764287+khale913@users.noreply.github.com>
2022-09-26 17:37:26 -0700

    typing working

--- src/app/components/landing/landing.component.ts
+++ src/app/components/landing/landing.component.ts
@@ -8,7 +8,7 @@ import { Component, OnInit } from '@angular/core';
 export class LandingComponent implements OnInit {
   typedTextSpan: any;
   cursorSpan: any;
-  textArray: any = ["hard", "fun", "a journey", "LIFE"];
+  textArray: any = ["REAL INTELLIGENCE", "AUTOMATED EXECUTION", "CONVERSATIONAL MARKETING", "BETTER DATA", "BETTER EFFECIENCY", "BETTER MARGINS"];
 
   typingDelay: number = 200;
   erasingDelay: number = 100;
@@ -32,17 +32,17 @@ export class LandingComponent implements OnInit {
   type() {
     this.cursorSpan = document.querySelector('.cursor');
     this.typedTextSpan = document.querySelector('.typed-text');
-    console.log('type', this.typedTextSpan, this.charIndex)
+    console.log('type', this.textArrayIndex, this.charIndex)
     if (this.charIndex < this.textArray[this.textArrayIndex].length) {
-      if (!this.cursorSpan.classList.contains("typing")) {
+      if (!this.cursorSpan.classList.contains("typing"))
         this.cursorSpan.classList.add("typing");
-        this.typedTextSpan.textContent += this.textArray[this.textArrayIndex].charAt(this.charIndex);
-        this.charIndex++;
-        setTimeout(() => {
-          this.type()
-          console.log('here')
-        }, this.typingDelay);
-      }
+      this.typedTextSpan.textContent += this.textArray[this.textArrayIndex].charAt(this.charIndex);
+      this.charIndex++;
+      setTimeout(() => {
+        this.type()
+        console.log('here')
+      }, this.typingDelay);
+
     } else {
       this.cursorSpan.classList.remove('typing');
 
@@ -56,23 +56,27 @@ export class LandingComponent implements OnInit {
 
   erase() {
     console.log('erase');
-    if (this.charIndex > 0) {
-
-      if (!this.cursorSpan.classList.contains('typing')) {
+    if (this.charIndex >= 0) {
+      console.log('boo');
+      if (!this.cursorSpan.classList.contains('typing'))
         this.cursorSpan.classList.add('typing');
-        this.typedTextSpan.textContent = this.textArray[this.textArrayIndex].substring(0, this.charIndex - 1);
-        this.charIndex--;
-        setTimeout(() => {
-          this.erase()
-        }, this.erasingDelay);
-      }
+      this.typedTextSpan.textContent = this.textArray[this.textArrayIndex].substring(0, this.charIndex - 1);
+      this.charIndex--;
+      setTimeout(() => {
+        this.erase()
+      }, this.erasingDelay);
+
 
     } else {
       this.cursorSpan.classList.remove('typing');
       this.textArrayIndex++
+      console.log('next')
+      setTimeout(() => {
+        this.type()
+      }, this.typingDelay + 1100);
       if (this.textArrayIndex >= this.textArray.length) {
         this.textArrayIndex = 0;
-
+        console.log('asss')
         setTimeout(() => {
           this.type()
         }, this.typingDelay + 1100);
