3954ec6e7962339b9c474797f79f7167b3779111
Kelton Hale <77764287+khale913@users.noreply.github.com>
2022-10-05 13:21:35 -0700

    cursor service on demo

--- src/app/components/demo/demo.component.ts
+++ src/app/components/demo/demo.component.ts
@@ -1,15 +1,22 @@
 import { Component, OnInit } from '@angular/core';
-
+import { CursorService } from 'src/app/SERVICE/cursor.service';
 @Component({
   selector: 'app-demo',
   templateUrl: './demo.component.html',
   styleUrls: ['./demo.component.scss']
 })
 export class DemoComponent implements OnInit {
-
-  constructor() { }
+  hover: boolean = false;
+  constructor(private service: CursorService) { }
 
   ngOnInit(): void {
+    this.service.hover.subscribe(c => {
+      this.hover = c;
+    })
+  }
+
+  hoverFunction(bool: boolean) {
+    this.service.hoverElement(bool);
   }
 
 }
