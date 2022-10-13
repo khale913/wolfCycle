3f995d224aa1d85d200f8e3dbc8c95f567cfa5c5
Kelton Hale <77764287+khale913@users.noreply.github.com>
2022-10-13 10:47:38 -0700

    preparing element detection

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,20 +1,36 @@
-import { Component, HostListener, Renderer2, ViewChild } from '@angular/core';
+import { Component, HostListener, Renderer2, ViewChild, ElementRef } from '@angular/core';
 import { CursorService } from './SERVICE/cursor.service';
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'wolfcycle-website';
+  @ViewChild('landingDiv', { static: false })
+  private landingDiv!: ElementRef<HTMLDivElement>;
+  isLandingDivScrolledIntoView!: boolean;
 
+  @HostListener('window:scroll', ['$event'])
+  isScrolledIntoView() {
+    if (this.landingDiv) {
+      const rect = this.landingDiv.nativeElement.getBoundingClientRect();
+      const topShown = rect.top >= 0;
+      const bottomShown = rect.bottom <= window.innerHeight;
+      this.isLandingDivScrolledIntoView = topShown && bottomShown;
+    }
+  }
+
+  title = 'wolfcycle-website';
   hover: boolean = false;
   public slides = [
     { src: "https://images.unsplash.com/photo-1567103472667-6898f3a79cf2?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHx0b3BpYy1mZWVkfDN8eGpQUjRobGtCR0F8fGVufDB8fHx8&auto=format&fit=crop&w=800&q=60" },
     { src: "https://images.unsplash.com/photo-1568819646628-64cd7b08617e?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHx0b3BpYy1mZWVkfDJ8eGpQUjRobGtCR0F8fGVufDB8fHx8&auto=format&fit=crop&w=800&q=60" },
     { src: "https://images.unsplash.com/photo-1664992892797-781fcf7b7541?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHx0b3BpYy1mZWVkfDF8eGpQUjRobGtCR0F8fGVufDB8fHx8&auto=format&fit=crop&w=800&q=60" },
-
   ];
+
+
+
   constructor(private renderer: Renderer2, private service: CursorService) { }
   @HostListener('mousemove', ['$event'])
   mouseMove(e: MouseEvent) {
